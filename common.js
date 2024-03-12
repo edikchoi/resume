f9adce45469afd0d6e2215b2d71e93660994c4c7
edik <edik.choi@gmail.com>
2024-03-12 17:57:09 +0900

    new

--- common.js
+++ common.js
@@ -1,3 +1,15 @@
 $('h1').click(function() {
     window.print();
 });
+
+$(document).ready(function() {
+    $('.info-wrap').each(function() {
+        if ($(this).eq(0).find('p').is(':contains(구축)')) {
+            $(this).find('.info:first').addClass('new');
+        } else if ($(this).eq(0).find('p').is(':contains(운영)')) {
+            $(this).find('.info:first').addClass('manage');
+        } else if ($(this).eq(0).find('p').is(':contains(개선)')) {
+            $(this).find('.info:first').addClass('develop');
+        };
+    });
+});
